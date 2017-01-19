da97d91f9acea49cc8dc8a83a297ab2959eec51f
fol and js <js@>
2017-01-19 14:57:42 -0800

    add new features to the function

--- js/scripts.js
+++ js/scripts.js
@@ -6,35 +6,44 @@ $(function() {
     var userInput = parseInt($('input#inputUser').val());
 
 
-
-
-
     var romanNumbers = ['I','V','X','L','C','D','M']
     var arabicNumbers = ['1','5','10','50','100','500','1000']
 
 // Back-End Logic
 
 var convert = function(number, position) {
-  if (value < 4 ) { romanNumbers[6].repeat(2)  }
-})
+  if (number < 4 ) {
+
+    alert(romanNumbers[position].repeat(number));
+
+  } else if ( number === 4 ) {
+
+    alert(romanNumbers[position] + romanNumbers[position + 1]);
+
+  } else if ( number === 5 ) {
+
+    alert( romanNumbers[number] );
+
+
+  }
+
+
+};
 
 
 
 
 
 var arabicThousands = Math.floor(userInput / 1000);
-alert(arabicThousands);
 var arabicHundreds = Math.floor((userInput - arabicThousands * 1000) / 100);
-alert(arabicHundreds);
 var arabicTens = Math.floor((userInput - (arabicThousands * 1000 + arabicHundreds * 100)) / 10);
-alert(arabicTens);
 var arabicOnes = Math.floor((userInput - (arabicThousands * 1000 + arabicHundreds * 100 + arabicTens * 10)) / 1);
-alert(arabicOnes);
 
 
-// var romanThousands = convert(arabicThousands, romanNumbers[6])
-// var romanHundreds =
-// var romanTens =
-// var romanOnes =
+
+var romanThousands = convert(arabicThousands, 6);
+var romanHundreds = convert(arabicHundreds, 4);
+var romanTens = convert(arabicTens, 2);
+var romanOnes = convert(arabicOnes, 0);
   });
 });
